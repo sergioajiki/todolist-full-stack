01ba0bbf6c179ae0346651b8acc6948b52bb0025
Sergio <sergioajk@hotmail.com>
2023-09-05 16:43:32 -0300

    criando type ServiceResponse

---
+++ backend/src/service/user.service.ts
@@ -0,0 +1,14 @@
+import { IUserModel } from '../interfaces/User/IUserModel';
+import { IUserPayload } from '../interfaces/User/IUserPayload';
+import { ServiceResponse } from '../interfaces/ServiceResponse';
+import UserModel from '../model/user.model';
+
+
+export default class UserService {
+  constructor(
+    private userModel: IUserModel = new UserModel()
+  )
+
+ 
+  
+}
