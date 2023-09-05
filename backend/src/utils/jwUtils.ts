7a8f3a0b232a179c68feebf35a21c7d8bce90833
Sergio <sergioajk@hotmail.com>
2023-09-05 19:53:27 -0300

    feat: jwUtils

--- backend/src/utils/jwUtils.ts
+++ backend/src/utils/jwUtils.ts
@@ -1,2 +1,19 @@
 import jwt from 'jsonwebtoken';
+import { TokenPayload } from '../interfaces/Token';
+import { Secret } from 'jsonwebtoken';
+import 'dotenv/config';
+
+export default class JwtUtils {
+    private static jwtSecret = process.env.JWT_SECRET as Secret
+    private static jwtOptions = { expiresIn: '30m'}
+
+    public static sign(payload: TokenPayload): string {
+      return jwt.sign(payload, this.jwtSecret, this.jwtOptions);
+    }
+  
+    public static verify(token: string): jwt.JwtPayload {
+      const decodedToken = jwt.verify(token, this.jwtSecret);
+      return decodedToken as TokenPayload;
+    }
+  }
 
