3ddd91f09a5f8f93e5181de4173ae1c4250a7ca5
Sergio <sergioajk@hotmail.com>
2023-09-05 14:46:00 -0300

    criando user.model

---
+++ backend/src/model/user.model.ts
@@ -0,0 +1,35 @@
+import SequelizeUsers from '../database/model/sequelizeUsers';
+
+import { IUser } from '../interfaces/User/IUser';
+import { IUserModel } from '../interfaces/User/IUserModel';
+import { IUserPayload } from '../interfaces/User/IUserPayload';
+
+export default class UserModel implements IUserModel {
+  private model = SequelizeUsers;
+
+  async getUserByEmail(email: string): Promise<IUser | null> {
+    const user = await this.model.findOne({
+      where: { email }
+    })
+    return !user ? null : user;
+  }
+
+  async createUser(userPayload: IUser): Promise<IUser> {
+    const newUser = await this.model.create(userPayload);
+    return newUser;
+  }
+
+  async updateUserById(id: number, userPayload: IUserPayload): Promise<number | IUser> {
+    const [updatedUser] = await this.model.update(userPayload, {
+      where: { id },
+    })
+    return updatedUser
+  }
+
+  async activateUser(id: number): Promise<number | IUser> {
+    const [updatedUser] = await this.model.update({ status: 1 }, {
+      where: { id },
+    })
+    return updatedUser
+  }
+}
